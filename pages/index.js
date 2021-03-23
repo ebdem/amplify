1b5acefc8f5b6a7d8fb410117a44bde46f82c864
ebdem <ebu.bekir.3333@gmail.com>
2021-03-23 16:19:39 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import {API} from 'aws-amplify';
+import { listPosts } from '../graphql/queries';
+
+export default function Home(){
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchPosts()
+  }, [])
+  async function fetchPosts(){
+    const postData = await API.graphql({query : listPosts});
+    setPosts(postData.data.listPosts.items);
+
+  }
+
+  return(
+    <div>
+      <h1 className="text-3x1 font-semibold tracking-wide mt-6 mb-2">Posts</h1>
+      {
+        posts.map((post, index) => (
+          <Link key={index} href={`/posts/${post.id}`}>
+            <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
+              <h2 className="text-xl font-semibold">{post.name}</h2>
+              <p className="text-gray-500 mt-2"> Author: {post.username}</p>
+            </div>
+
+          </Link>
+        ) )
+      }
+    </div>
+  )
+
+}
